e02f3096947f9275baff975af848d93da6bb516b
jack <62025739+1602077@users.noreply.github.com>
2023-08-13 17:42:28 +0100

    deploy: istio-base base customization

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -37,5 +37,6 @@ print-header "registering ${GITHUB_REPO} to argo"
 argocd repo add ${GITHUB_REPO}
 
 print-header "creating argo components"
-kubectl apply -f ./component/gitops-controller/
-# kubectl apply -f ./component/app/
+kubectl apply -f ./components/main.yaml
+kubectl apply -f ./components/gitops-controller/
+kubectl apply -f ./components/apps/
