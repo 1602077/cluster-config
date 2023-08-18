609aba113286f348f4f42ec8a211497db730d97e
jack <62025739+1602077@users.noreply.github.com>
2023-08-18 20:42:33 +0100

    refactor: modular tf structure

---
+++ terraform/build/terraform.tfvars
@@ -0,0 +1,6 @@
+project_id         = "REPLACE_ME"
+region             = "us-central1"
+cluster_name       = "gitops-argo-testing"
+primary_node_count = 1
+vpc_name           = "gitops-argo-testing"
+subnet_name        = "gitops-argo-testing-subnet"
---
+++ terraform/build/variables.tf
@@ -0,0 +1,27 @@
+variable "project_id" {
+  description = "gcp project id"
+}
+
+variable "region" {
+  description = "region that cluster and vpc are located in"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "name of cluster, unique within project & zone"
+  default     = "primary"
+}
+
+
+variable "primary_node_count" {
+  description = "initial node count of primary node pool"
+  default     = 1
+}
+
+variable "vpc_name" {
+
+}
+
+variable "subnet_name" {
+
+}
