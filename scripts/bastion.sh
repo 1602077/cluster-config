93e712f374975e35f92ee162ca3cbe2828111b7d
jack <62025739+1602077@users.noreply.github.com>
2023-09-02 12:43:54 +0100

    refactor: auto-discconect from proxy

--- scripts/bastion.sh
+++ scripts/bastion.sh
@@ -22,17 +22,11 @@ gcloud compute ssh $BASTION_HOST \
 	--zone=$CLUSTER_ZONE \
 	--ssh-flag="-4 -L8888:localhost:$HTTPS_PROXY_PORT -N -q -f"
 
-print-header "spawning sub-shell using HTTPS_PROXY"
+print-header "spawning sub-shell using HTTPS_PROXY to connect to $CROSSPLANE_CLUSTER cluster"
 HTTPS_PROXY="localhost:$HTTPS_PROXY_PORT" \
 	PS1='\e[0;36m($(kubectl config current-context))\e[0m $ ' \
 	/bin/bash
 
-CLEANUP_CMD="ps aux | grep '[8]888' | awk '{print \$2}' | xargs kill"
+ps aux | grep '[8]888' | awk '{print $2}' | xargs kill
 
-echo "$CLEANUP_CMD" | pbcopy
-
-command cat <<EOF
-
-To stop listening on remote client run (added to clipboard):
-$CLEANUP_CMD
-EOF
+print-header "disconnected from remote client"
